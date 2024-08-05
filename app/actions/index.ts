b4292e7311058fbf32e8704b6aa61bb561820990
Pablo Joaquín Giménez Martínez <joaquingim77@gmail.com>
2024-08-05 19:36:44 -0300

    Fixed doLogin action 3

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -3,11 +3,15 @@
 import { signIn, signOut } from "../lib/auth";
 
 export async function doLogin(formData: FormData) {
-    const action = formData.get("action")!;
-    await signIn(action, { redirectTo: "/dashboard" });
+    const action = formData.get("action");
+    // Ensure action is a string and not null
+    if (typeof action === "string") {
+        await signIn(action, { redirectTo: "/dashboard" });
+    } else {
+        console.error("La accion no es valida perro");
+    }
     console.log(action);
 }
-
 export async function doLogout() {
     await signOut({ redirectTo: "/" });
 }
